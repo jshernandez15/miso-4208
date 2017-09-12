0871a6dc2eb1e2014334d631ad7247cc12df7922
Felipe Martinez <f.martinez2@uniandes.edu.co>
2017-09-11 19:40:17 -0500

    Evidencias todas las app

---
+++ Documentacion/Sprint-1/reportes/Evidencias Habitica Web/01.js
@@ -0,0 +1,30 @@
+function monkey() {
+    function callback() {
+        gremlins.createHorde()
+        .before(function(done) {
+            var horde = this;
+            setTimeout(function(){
+                horde.log('async');
+                done();
+            }, 500);
+        })
+        .before(function() {
+            this.log('sync');
+        })
+        .gremlin(gremlins.species.formFiller())
+        .gremlin(gremlins.species.clicker())
+        .gremlin(gremlins.species.scroller())
+        .gremlin(gremlins.species.typer())
+        .mogwai(gremlins.mogwais.alert())
+        .mogwai(gremlins.mogwais.gizmo().maxErrors(10))
+        .unleash();
+    }
+    var s = document.createElement("script");
+    s.src = "https://rawgithub.com/marmelab/gremlins.js/master/gremlins.min.js";
+    if (s.addEventListener) {
+        s.addEventListener("load", callback, false)
+    } else if (s.readyState) {
+        s.onreadystatechange = callback
+    }
+    document.body.appendChild(s);
+}
