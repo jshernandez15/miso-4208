6503fd4d86d342c3a77663c92de6e02c6b713f2a
Felipe Martinez <Felipe.MartinezGonzales@wnco.com>
2017-10-18 00:25:34 -0500

    GNU Cash Android Calabash

--- Documentacion/Sprint-2/Calabash/bdd-gnucash/features/step_definitions/calabash_steps.rb
+++ Documentacion/Sprint-2/Calabash/bdd-gnucash/features/step_definitions/calabash_steps.rb
@@ -16,3 +16,13 @@ end
 Given "the account \"Calabash Account\" is listed" do
     check_element_exists("* text:'Calabash Account'")
 end
+
+Given "I press the New Transaction Floating Button" do
+    touch("* id:'fab_create_transaction'") 
+end
+
+Given "I go to my wallet" do
+    touch("* text:'Assets'")
+    touch("* text:'Current Assets'")
+    touch("* text:'Cash in Wallet'")
+end
